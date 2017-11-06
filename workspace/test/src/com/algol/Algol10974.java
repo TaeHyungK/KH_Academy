035f3f059f68047d3a0adb2b437b6f89f92429bf
taehyung <rlaxogud555@gmail.com>
2017-11-06 20:42:29 +0900

    알골10974 완료 / 1107, 1722 미완료

--- workspace/test/src/com/algol/Algol10974.java
+++ workspace/test/src/com/algol/Algol10974.java
@@ -1,66 +1,59 @@
 package com.algol;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Algol10974 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		
 		int how = sc.nextInt();
 		int[] soon = new int[how];
-
-		ArrayList<int[]> list = new ArrayList<int[]>();
-
+		
 		for(int i=0;i<how;i++) {
 			soon[i] = i+1;
 		}
-		list.add(soon);
-
-		System.out.println();
-
-		for(int k=0; k<soon.length; k++) {
-			for(int i=soon.length-1; i>0; i--) {
-				if(soon[i] > soon[i-1]) {
-					for(int j=soon.length-1; j>=i-1; j--) {
-						if(soon[i-1] < soon[j]) {
-							swap(soon, i-1, j);
-							reverseArrayInt(soon, i, soon.length);
-
-						}
-					}
-				}
-			}	
-			
-			list.add(soon);
-
-			for(int temp : soon) {
-				System.out.print(temp + " ");
+		
+		do {
+			for(int i=0;i<how;i++) {
+				System.out.print(soon[i] + " ");
 			}
 			System.out.println();
+		}while(next(soon));
+		
+	}
+	
+	public static boolean next(int[] temp) {
+		int i = temp.length-1;
+		
+		while(i>0 && temp[i-1] >= temp[i]) {
+			i -= 1;
 		}
-
-
-
-	}
-
-	public static void swap(int[] soon, int index1, int index2) {
-		int temp = soon[index1];
-		soon[index1] = soon[index2];
-		soon[index2] = temp;
-	}
-
-	public static void reverseArrayInt(int[] soon, int start, int end) {
-		int left = start;
-		int right = end-1;
-
-		while(left < right) {
-			int temp = soon[left];
-			soon[left] = soon[right];
-			soon[right] = temp;
-
-			left++;
-			right--;
+		
+		if(i<=0) {
+			return false;
 		}
+		
+		int j= temp.length-1;
+		
+		while(temp[j] <= temp[i-1]) {
+			j-=1;
+		}
+		
+		int changed = temp[i-1];
+		temp[i-1] = temp[j];
+		temp[j] = changed;
+		
+		j = temp.length-1;
+		while(i < j) {
+			changed = temp[i];
+			temp[i] = temp[j];
+			temp[j] = changed;
+			i+= 1;
+			j-= 1;
+		}
+		
+		return true;
 	}
+	
+
 }
---
+++ workspace/test/src/com/algol/Algol1107.java
@@ -0,0 +1,22 @@
+package com.algol;
+
+import java.util.Scanner;
+
+public class Algol1107 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int current = 100;
+		
+		int num = sc.nextInt();
+		int count_btn = sc.nextInt();
+		
+		int[] gojang_btn = new int[count_btn];
+		for(int i=0;i<count_btn;i++) {
+			gojang_btn[i] = sc.nextInt();
+		}
+		
+		
+		
+	}
+}
