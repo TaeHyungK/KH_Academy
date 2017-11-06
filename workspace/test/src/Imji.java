4778be433bb46333b7a8cd2856e1bd5887f6632e
taehyung <rlaxogud555@gmail.com>
2017-11-06 18:57:01 +0900

    2017.11.06 jQuery 선택자, 배열, dom

--- workspace/test/src/Imji.java
+++ workspace/test/src/Imji.java
@@ -1,67 +1,60 @@
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Imji {
+
 	public static void main(String[] args) {
-		int a;
-		int b;
-		int c;
-		int cnt_0 = 0;
-		int cnt_1 = 0;
-		int cnt_2 = 0;
-		int cnt_3 = 0;
-		int cnt_4 = 0;
-		int cnt_5 = 0;
-		int cnt_6 = 0;
-		int cnt_7 = 0;
-		int cnt_8 = 0;
-		int cnt_9 = 0;
-
+		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
-
-		a = sc.nextInt();
-
-		b = sc.nextInt();
-
-		c = sc.nextInt();
-
-		int result = a*b*c;
-		int result_Num;
-
-		for (int i = 0; i < String.valueOf(result).length(); i++) {
-			result_Num = String.valueOf(result).charAt(i);
-			if(result_Num == '0') {
-				cnt_0++;
-			}else if(result_Num == '1') {
-				cnt_1++;
-			}else if(result_Num == '2') {
-				cnt_2++;
-			}else if(result_Num == '3') {
-				cnt_3++;
-			}else if(result_Num == '4') {
-				cnt_4++;
-			}else if(result_Num == '5') {
-				cnt_5++;
-			}else if(result_Num == '6') {
-				cnt_6++;
-			}else if(result_Num == '7') {
-				cnt_7++;
-			}else if(result_Num == '8') {
-				cnt_8++;
-			}else if(result_Num == '9') {
-				cnt_9++;
-			}         
+		
+		int size, x, a, b, count;
+		System.out.print("길이 입력: ");
+		size = sc.nextInt();
+		
+		int [] map = new int [size];
+		int [] map2 = new int [size];
+		
+		for(int i=0; i<map.length; i++) {
+			map[i] = map.length+10;
 		}
-		System.out.println(cnt_0);
-		System.out.println(cnt_1);
-		System.out.println(cnt_2);
-		System.out.println(cnt_3);
-		System.out.println(cnt_4);
-		System.out.println(cnt_5);
-		System.out.println(cnt_6);
-		System.out.println(cnt_7);
-		System.out.println(cnt_8);
-		System.out.println(cnt_9);
-	}   
+		
+		for(int k=0; k<map.length; k++) {
+			map2[k] = (int)(Math.random()*10);
+		}
+		
+		for(int r=0; r<map.length; r++) {
+				if(map[r] == map.length+10) {
+					System.out.print("#");
+				}  
+			}
+		do {
+			System.out.println();
+			System.out.print("값을 입력하세요: ");
+			a = sc.nextInt();
+				
+			for(int i=0; i<map2.length; i++) {
+				if(a == map2[i]) {
+					map[i] = map2[i];
+					System.out.print(map[i]);
+				} else if(map[i] == map.length+10) {
+					System.out.print("#");
+				} 
+			}
+			
+			boolean exit = true;
+			for(int k=0; k<map.length; k++) {
+				if(map[k] == map.length+10) {
+					exit = false;
+					break;
+				}
+			}
+			
+			if(exit) {
+				a = 10;
+			}
+					
+		} while(a != 10);
+		System.out.println();
+		System.out.println("프로그램 종료");
+	}
 }
